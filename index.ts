58af185280256a794d98f9971a893784294b6a72
syeda <sbtshakeel61@gmail.com>
2024-05-28 00:29:16 +0500

    enum in string

---
+++ index.ts
@@ -0,0 +1,7 @@
+//now string value
+enum Days {
+    Monday  = "Sunny Weather",
+    Tuesday = "Rainy Weather",
+}
+console.log(Days.Monday); //output assign value string {sunny weather}
+console.log(Days.Tuesday); //output assign value string {rainy weather}
